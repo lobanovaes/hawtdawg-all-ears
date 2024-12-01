23be063f8f493800db3394f496969aebf2816688
ArgelTal <dimonchikgvd@gmail.com>
2024-12-01 13:51:21 +0700

    Revert "init commit"
    This reverts commit a07ed75fe45b3f0f84431ae28563f660b2e6619b.

--- pytest_example.py
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-import pytest
-
-
-def test_something(data: Any = 12):
-    assert isinstance(data, int)
--- version.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# !/usr/bin/env python3
-
-VERSION = '0.0.1a'
